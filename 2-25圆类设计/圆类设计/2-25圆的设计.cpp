113ac48ec33bf8fe9b22d8768e6de5fbe362dc1e
fwqaq <45515058+fwqaq@users.noreply.github.com>
2019-02-25 17:09:53 +0800

    圆类的设计并且判断圆点之间的关系

---
+++ 2-25圆类设计/圆类设计/2-25圆的设计.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+//点类
+class Point{
+public:
+	void setX(int x){
+		m_X = x;
+	}
+	void setY(int y){
+		m_Y = y;
+	}
+	int getX(){
+		return m_X;
+	}
+	int getY(){
+		return m_Y;
+	}
+private:
+	int m_X;
+	int m_Y;
+};
+//圆类
+class Circle{
+public:
+	//设置半径
+	void setR(int R){
+		m_R = R;
+	}
+	//获取半径
+	int getR(){
+		return m_R;
+	}
+	//设置圆心
+	void setCenter(Point p){
+		m_Center = p;
+	}
+	//获得圆心
+	Point getCenter(){
+		return m_Center;
+	}
+private:
+	int m_R;
+	Point m_Center;
+};
+//利用全局变量判断点和圆的距离
+void isL(Circle c, Point p){
+	//点到圆心的距离的平方
+	int away = (c.getCenter().getX() - p.getX())*(c.getCenter().getX() - p.getX()) + (c.getCenter().getY() - p.getY())*(c.getCenter().getY() - p.getY());
+	//半径平方的距离
+	int distance = c.getR()*c.getR();
+	if (away > distance){
+		cout << "点在圆外" << endl;
+	}
+	else if (away < distance){
+		cout << "点在圆内" << endl;
+	}
+	else{
+		cout << "点在圆上" << endl;
+	}
+}
+int main(){
+	cout << "abcd" << endl;
+	system("pause");
+	return EXIT_SUCCESS;
+}
