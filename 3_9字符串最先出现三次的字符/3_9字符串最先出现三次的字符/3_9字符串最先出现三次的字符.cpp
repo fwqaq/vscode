cbc628441317c21225a5fc1a22d9fb5f224ce4db
fwqaq <45515058+fwqaq@users.noreply.github.com>
2019-03-09 21:09:50 +0800

    字符串中最先出现三次的字符串

---
+++ 3_9字符串最先出现三次的字符/3_9字符串最先出现三次的字符/3_9字符串最先出现三次的字符.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string str;
+	getline(cin, str);
+	char c[256] = { 0 };
+	for (unsigned int i = 0; i < str.length(); i++)
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			c[str[i]]++;
+			if (c[str[i]] == 3)
+			{
+				cout << str[i] << endl;
+				break;
+			}
+		}
+	}
+	system("pause");
+	return 0;
+
+}
