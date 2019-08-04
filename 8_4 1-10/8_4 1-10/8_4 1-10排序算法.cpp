383b6b703753e89046d45e66ea217968a5834f43
fwqaq <45515058+fwqaq@users.noreply.github.com>
2019-08-05 00:01:22 +0800

    牛客网1-10排序

---
+++ 8_4 1-10/8_4 1-10/8_4 1-10排序算法.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <list>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+	vector<int> vtNums;
+	int nTemp = 0;
+	cin >> nTemp;
+	cout << nTemp;
+
+	return 0;
+}
